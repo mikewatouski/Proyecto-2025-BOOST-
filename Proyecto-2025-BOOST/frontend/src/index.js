8ee08b6499b25a14f4dc3ad4f8509cbe84ea2cd2
JulianRozenfarb <julirozenfarb@gmail.com>
2025-06-10 11:37:06 -0300

    cambios vol 4954854

--- Proyecto-2025-BOOST/frontend/src/index.js
+++ Proyecto-2025-BOOST/frontend/src/index.js
@@ -1,10 +1,11 @@
-const PORT = process.env.PORT || 3001;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-
-app.get('/mensaje', (req, res) => {
-  res.json([{ texto: "¡Hola desde el backend real!" }]);
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
