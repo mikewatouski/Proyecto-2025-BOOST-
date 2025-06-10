1b656bd0011f9ded4f4b722d6e7a5595b93b7471
JulianRozenfarb <julirozenfarb@gmail.com>
2025-06-10 11:17:33 -0300

    cors eliminado

--- Proyecto-2025-BOOST/frontend/src/index.js
+++ Proyecto-2025-BOOST/frontend/src/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
 
 app.get('/mensaje', (req, res) => {
   res.json([{ texto: "¡Hola desde el backend real!" }]);
