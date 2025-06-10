9acd6742e6193b7f0aa918a7319f648023e4f8fd
JulianRozenfarb <julirozenfarb@gmail.com>
2025-06-10 11:05:32 -0300

    cambios para vercel

--- Proyecto-2025-BOOST/frontend/src/index.js
+++ Proyecto-2025-BOOST/frontend/src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+app.use(cors());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.get('/mensaje', (req, res) => {
+  res.json([{ texto: "¡Hola desde el backend real!" }]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
